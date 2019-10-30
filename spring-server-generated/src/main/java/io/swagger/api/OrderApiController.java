17bbdec0ef5b54107410efefa008122b951a4283
Fan Fan <fan.fa@husky.neu.edu>
2019-10-29 18:31:38 -0700

    add order and pizza services

--- spring-server-generated/src/main/java/io/swagger/api/OrderApiController.java
+++ spring-server-generated/src/main/java/io/swagger/api/OrderApiController.java
@@ -2,13 +2,30 @@ package io.swagger.api;
 
 import io.swagger.annotations.ApiParam;
 import io.swagger.models.Order;
+import io.swagger.service.OrderService;
+import javax.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RequestMapping("/pizza/v1")
+@RestController
 public class OrderApiController implements io.swagger.apis.OrderApi{
+
+  @Autowired private OrderService orderService;
 
   @Override
   public ResponseEntity<Order> getAOrder(@ApiParam(value = "order id",required=true) @PathVariable("id") String id) {
-    return null;
+    return new ResponseEntity<Order>(orderService.getOrderById(id), HttpStatus.OK);
+  }
+
+  @Override
+  public ResponseEntity<Order> makeOrder(@ApiParam(value = "Order to add"  )  @Valid @RequestBody Order order) {
+    Order result = orderService.addOrder(order);
+    return new ResponseEntity<Order>(result, HttpStatus.OK);
   }
 }
--- spring-server-generated/src/main/java/io/swagger/api/PizzaApiController.java
+++ spring-server-generated/src/main/java/io/swagger/api/PizzaApiController.java
@@ -1,54 +1,31 @@
 package io.swagger.api;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import io.swagger.annotations.*;
-import io.swagger.model.Pizza;
-import java.io.IOException;
-import javax.servlet.http.HttpServletRequest;
+import io.swagger.annotations.ApiParam;
+import io.swagger.models.Pizza;
+import io.swagger.service.PizzaService;
 import javax.validation.Valid;
-import javax.validation.constraints.*;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@javax.annotation.Generated(value = "io.swagger.codegen.languages.SpringCodegen", date = "2019-10-19T05:03:41.076Z")
+@RequestMapping("/pizza/v1")
+@RestController
+public class PizzaApiController implements io.swagger.apis.PizzaApi{
 
-@Controller
-public class PizzaApiController implements PizzaApi {
+  @Autowired private PizzaService pizzaService;
 
-    private static final Logger log = LoggerFactory.getLogger(PizzaApiController.class);
+  @Override
+  public ResponseEntity<Pizza> getAPizza(@ApiParam(value = "order id",required=true) @PathVariable("id") String id) {
+    return new ResponseEntity<Pizza>(pizzaService.getPizzaById(id), HttpStatus.OK);
+  }
 
-    private final ObjectMapper objectMapper;
-
-    private final HttpServletRequest request;
-
-    @org.springframework.beans.factory.annotation.Autowired
-    public PizzaApiController(ObjectMapper objectMapper, HttpServletRequest request) {
-        this.objectMapper = objectMapper;
-        this.request = request;
-    }
-
-    public ResponseEntity<Void> addPizza(@ApiParam(value = "Pizza to add"  )  @Valid @RequestBody Pizza pizza) {
-        String accept = request.getHeader("Accept");
-        return new ResponseEntity<Void>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
-    public ResponseEntity<Pizza> getAPizza(@ApiParam(value = "pizza id",required=true) @PathVariable("id") String id) {
-        String accept = request.getHeader("Accept");
-        if (accept != null && accept.contains("application/json")) {
-            try {
-                return new ResponseEntity<Pizza>(objectMapper.readValue("{  \"displayName\" : \"displayName\",  \"isGlutenFree\" : true,  \"toppings\" : [ { }, { } ],  \"id\" : \"id\",  \"type\" : { },  \"ownerId\" : \"ownerId\"}", Pizza.class), HttpStatus.NOT_IMPLEMENTED);
-            } catch (IOException e) {
-                log.error("Couldn't serialize response for content type application/json", e);
-                return new ResponseEntity<Pizza>(HttpStatus.INTERNAL_SERVER_ERROR);
-            }
-        }
-
-        return new ResponseEntity<Pizza>(HttpStatus.NOT_IMPLEMENTED);
-    }
-
+  @Override
+  public ResponseEntity<Pizza> addPizza(@ApiParam(value = "Order to add"  )  @Valid @RequestBody Pizza pizza) {
+    Pizza result = pizzaService.addPizza(pizza);
+    return new ResponseEntity<Pizza>(result, HttpStatus.OK);
+  }
 }
