2bc58e91d1d5f650c828a156dda06f32db8a3f79
Lu bao <bao.lu1@husky.neu.edu>
2019-11-26 15:12:06 +1100

    add corsconfiguration

--- spring-server-generated/src/test/java/io/swagger/api/GenerateReceiptApiControllerTest.java
+++ spring-server-generated/src/test/java/io/swagger/api/GenerateReceiptApiControllerTest.java
@@ -42,8 +42,6 @@ public class GenerateReceiptApiControllerTest {
   private OrderRepository orderRepository;
 
   @Autowired
-  private PriceService privateService;
-  @Autowired
   private GenerateReceiptApi api;
 
 
