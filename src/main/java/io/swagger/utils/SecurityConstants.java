2350eadec59defdf2cbece298b1f3307aa8dc513
Haiqing Wang <haiqingwang1005@gmail.com>
2020-03-31 17:22:14 -0700

    Change domain name

--- src/main/java/io/swagger/utils/SecurityConstants.java
+++ src/main/java/io/swagger/utils/SecurityConstants.java
@@ -7,7 +7,9 @@ public class SecurityConstants {
     public static final String TOKEN_PREFIX = "Bearer ";
     public static final String HEADER_STRING = "Authorization";
     public static final String REGISTER_URL = "/pizza/register";
-    public static final String DOMAIN = "https://haiqing-pizza-web.herokuapp.com";
-    //public static final String DOMAIN = "http://localhost:3000";
+    public static final String DOMAIN = "haiqing-pizza-web.herokuapp.com";
+    public static final String PORT = "";
+    //public static final String DOMAIN = "localhost";
+    //public static final String PORT = :3000"";
 
 }
