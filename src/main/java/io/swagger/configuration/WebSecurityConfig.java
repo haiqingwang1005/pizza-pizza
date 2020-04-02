e07f3a1694201f7c3b1ec70c39ff09e279bd3b21
Haiqing Wang <haiqingwang1005@gmail.com>
2020-04-01 19:13:18 -0700

    use heroku

--- src/main/java/io/swagger/configuration/WebSecurityConfig.java
+++ src/main/java/io/swagger/configuration/WebSecurityConfig.java
@@ -69,8 +69,8 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Bean
     protected HeaderWriter headerWriter() {
         return (httpServletRequest, httpServletResponse) -> {
-            //httpServletResponse.addHeader("Access-Control-Allow-Origin", "https://haiqing-pizza-web.herokuapp.com");
-            httpServletResponse.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
+            httpServletResponse.addHeader("Access-Control-Allow-Origin", "https://haiqing-pizza-web.herokuapp.com");
+            //httpServletResponse.addHeader("Access-Control-Allow-Origin", "http://localhost:3000");
             httpServletResponse.addHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
             httpServletResponse.addHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, PUT, DELETE, OPTIONS");
             httpServletResponse.addHeader("Access-Control-Allow-Credentials", "true");
